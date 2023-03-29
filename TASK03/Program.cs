76d174f8d2d48d5e7d9b95b245317ca55cd6b889
PAUL AGUREEFF <p.agureev@mail.ru>
2023-03-29 20:06:08 +0300

    Задачка 3 получилась

---
+++ TASK03/Program.cs
@@ -0,0 +1,51 @@
+﻿// Название дня недели по номеру
+
+Console.WriteLine("Введите число, которому соответствует день недели :");
+string day = Console.ReadLine();
+
+if (day == "1")
+{
+    Console.WriteLine("Опять понедельник");
+}
+else if (day == "2")
+{
+
+    Console.WriteLine("Уже вторник");
+}
+
+else if (day == "3")
+{
+
+    Console.WriteLine("Среда - маленькая пятница");
+}
+
+else if (day == "4")
+{
+
+    Console.WriteLine("Чисто Четверг!!!");
+}
+
+else if (day == "5")
+{
+
+    Console.WriteLine("Пятница - Стартуем!");
+}
+
+else if (day == "6")
+{
+
+    Console.WriteLine("Субботний расслабон");
+}
+
+else if (day == "7")
+{
+
+    Console.WriteLine("Воскресенье - Недели нет");
+}
+
+else
+{
+
+    Console.WriteLine("Да Вы Шалун! ;))))");    
+}
+
