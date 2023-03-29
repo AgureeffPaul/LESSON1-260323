a2193c98709206d8a8c2a35e147757f4e750e8c9
PAUL AGUREEFF <p.agureev@mail.ru>
2023-03-29 20:52:06 +0300

    Задачка 7 получилась

---
+++ TASK07/Program.cs
@@ -0,0 +1,16 @@
+﻿// Ввод трехзначное число, на выходе показать третью цифру этого числа
+
+Console.WriteLine("Введите трехзначное целое число:");
+int number = Convert.ToInt32(Console.ReadLine());
+int lastNumber =  number % 10;
+
+if (number >=100 && number <=1000)
+{
+Console.WriteLine($"Последняя цифра числа : {lastNumber}");
+}
+else
+{
+    Console.WriteLine($"Число {number} не трехзначное");
+}
+
+
