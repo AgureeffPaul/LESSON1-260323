ecc864cf132e5d3dc85b0b96bf59085f48bd0280
PAUL AGUREEFF <p.agureev@mail.ru>
2023-03-29 20:35:15 +0300

    Получилась задачка 5

--- TASK04/Program.cs
+++ TASK04/Program.cs
@@ -10,25 +10,29 @@ Console.WriteLine("Введите второе число:");
 int number2 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите третье число:");
 int number3 = Convert.ToInt32(Console.ReadLine());
+int max = number1;
+
+if (number1==number2)
+{
+Console.WriteLine($"Введённые числа равны"); // Проверка на равенство
+}
 
 
+// if (number1 > number2)
+//     {
+//         int max = number1;
+//     }
 
+// else if (number2 > number3)
+//         {
+//             int max = number2;
+//         }
+// else
+//         {
+//             int max = number3;
+//         }
 
-if (number1 > number2)
-    {
-        int max = number1;
-    }
-
-else if (number2 > number3)
-        {
-            int max = number2;
-        }
-else
-        {
-            int max = number3;
-        }
-
-Console.WriteLine($"Максимальное число : {max}"); // Выводим максимум
+// Console.WriteLine($"Максимальное число : {max}"); // Выводим максимум
 
     
 
---
+++ TASK05/Program.cs
@@ -0,0 +1,13 @@
+﻿// Вводим число N выводим все целые четные числа от -N до N
+
+
+Console.WriteLine("Введите положительное целое число:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int count = -number;
+
+while (count <= number)
+{
+    Console.Write ($"{count}, ");
+    count ++;
+}
