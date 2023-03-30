7e7d2e39374b8a1096335648720efa647e80f6a4
PAUL AGUREEFF <p.agureev@mail.ru>
2023-03-30 06:02:13 +0300

    Ура! Задача 4 получилась

--- TASK04/Program.cs
+++ TASK04/Program.cs
@@ -17,25 +17,17 @@ if (number1 == number2 && number2 == number3)
     Console.WriteLine($"Все введённые Вами числа равны друг другу"); // Проверка на равенство
 }
 
-
 if (number2 > max)
 {
-    int max = number2;
+    max = number2;
 }
-
-else if (number3 > max)
+if (number3 > max)
 {
-    int max = number3;
+    max = number3;
 }
 else
 {
-    int max = number1;
+    max = number1;
 }
 
-Console.WriteLine($"Максимальное число : {max}"); // Выводим максимум
-
-
-
-
-
-
+Console.WriteLine($"Максимальное введённое число : {max}");
