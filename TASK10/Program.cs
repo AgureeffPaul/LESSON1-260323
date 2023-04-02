3c007003dc509928f5e2174133331bc7a5118a55
PAUL AGUREEFF <p.agureev@mail.ru>
2023-04-02 22:52:40 +0300

    Исправил задачу 10. Добавил проверку на трехзначность

--- TASK10/Program.cs
+++ TASK10/Program.cs
@@ -4,16 +4,24 @@
 // 782 -> 8
 // 918 -> 1
 
-int numRandom = new Random().Next(100, 1000);
-Console.WriteLine($"Случайное число из диапазона 100-999 => {numRandom}");
 
-int newNum = ShowSecondDigit(numRandom);
-Console.WriteLine($"Вторая цифра случайного числа : {newNum}");
+Console.WriteLine($"Введите трехзначное число:");
+int num = Convert.ToInt32(Console.ReadLine());
 
-int ShowSecondDigit(int randomNuber)
-
+if (num < 100 || num > 1000)
 {
-    int action1 = randomNuber / 10;
+   Console.WriteLine("Вы ввели не трехзначное число"); 
+}
+
+else
+{
+    int newNum = ShowSecondDigit(num);
+    Console.WriteLine($"Вторая цифра введённого числа : {newNum}");
+}
+
+int ShowSecondDigit(int secondNumber)
+{
+    int action1 = secondNumber / 10;
     int action2 = action1 % 10;
     
     return action2;
