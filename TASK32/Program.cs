c7febca470798362758166da1de3d8cdd0c5cc2d
PAUL AGUREEFF <p.agureev@mail.ru>
2023-04-06 11:09:10 +0300

    Задача 32 работает

---
+++ TASK32/Program.cs
@@ -0,0 +1,47 @@
+﻿// Задача 32: Напишите программу замена элементов
+// массива: положительные элементы замените на
+// соответствующие отрицательные, и наоборот.
+// [-4, -8, 8, 2] -> [4, 8, -8, -2]
+// Задача 33: Задайте массив. Напишите программу, которая
+// определяет, присутствует ли заданное число в массиве.
+// 4; массив [6, 7, 19, 345, 3] -> нет
+// 3; массив [6, 7, 19, 345, 3] -> да
+
+int[] CreateArrayInt(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]}");
+    }
+    Console.Write("]");
+}
+
+void ChangeModuleSing(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = arr[i] * -1;
+    }
+}
+
+int[] array = CreateArrayInt(4, -10, 10);
+PrintArray(array);
+ChangeModuleSing(array);
+
+Console.WriteLine();
+Console.WriteLine($"Зеркальный массив :");
+PrintArray(array);
