bb3e08fcc087c1e0b369268019dddf6739ef9fde
PAUL AGUREEFF <p.agureev@mail.ru>
2023-04-18 18:45:08 +0300

    Задача 42. Перевод десятичных в двоичные числа

---
+++ TASK42/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 42. Перевод десятичного в двоичное число.
+
+Console.WriteLine("Введите целое число");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Двоичное число от числа {userNumber} равно {DecToBin (userNumber)}");
+
+int DecToBin (int num)
+{
+    int result = 0;
+    int count = 1;
+
+    while (num > 0)
+    {
+        result = result + num % 2 * count;
+        count *= 10;
+        num /= 2;
+    }
+    return result;
+}
