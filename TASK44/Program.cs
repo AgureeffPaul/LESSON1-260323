d15996556e9ba7b4a4116ceafb0648254f721f6b
PAUL AGUREEFF <p.agureev@mail.ru>
2023-04-18 22:46:37 +0300

    Задача 44. Числа Фибоначчи массив. Работает

---
+++ TASK44/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 44. Не используя рекурсию, выведите первые N чисел Фибоначчи.
+// Первые два числа Фибоначчи - 0 и 1.
+
+Console.WriteLine("Введите целое число N");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+
+PrintArray (FibboNumbers(userNumber));
+
+int[] FibboNumbers(int num)
+{
+    int[] fibboArray = new int[num];
+    fibboArray[0] = 0;
+    fibboArray[1] = 1;
+
+    for (int i = 2; i < num; i++)
+    {
+        fibboArray[i] = fibboArray[i - 1] + fibboArray[i - 2];
+    }
+    return fibboArray;
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]}");
+    }
+    Console.Write("]");
+}
