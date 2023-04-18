372a8c31f9625633c81b6497bc5f4b51dd432b06
PAUL AGUREEFF <p.agureev@mail.ru>
2023-04-18 17:12:45 +0300

    Задача 40 семинар. Треугольник. Работает

---
+++ TASK40/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 40. Три стороны вводим, проверяем, может ли быть такой треугольник.
+
+Console.WriteLine("Введите длину первой стороны");
+int firstSide = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите длину второй стороны");
+int secondSide = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите длину третьей стороны");
+int thirdSide = Convert.ToInt32(Console.ReadLine());
+
+bool result = IsTriangle(firstSide, secondSide, thirdSide);
+string res = result ? "Треугольник с данными сторонами может существовать" : "Треугольник с данными сторонами не существует";
+Console.WriteLine (res);
+
+bool IsTriangle (int num1, int num2, int num3)
+{
+    return num1 < num2 + num3 && num2 < num1 + num3 && num3 < num1 + num2;
+}
+
