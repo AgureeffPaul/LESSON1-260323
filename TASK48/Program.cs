1bd57d1c0f815864031d08409acb187ba22e81e1
PAUL AGUREEFF <p.agureev@mail.ru>
2023-04-19 18:01:23 +0300

    Задача 48. Заполнение массива м + n. Works

---
+++ TASK48/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задача 48. Задать двумерный массив Amn и заполнить его m + n
+
+Console.WriteLine("Введите количество строк");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите количество столбцов");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int[,] resultArray = CreateArrayMatrix (m, n);
+PrintMatrix(resultArray);
+
+
+int[,] CreateArrayMatrix(int rows, int columns)
+{
+    int[ , ] matrix = new int[rows, columns];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+        matrix[i, j] = i + j;
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+    Console.Write("|");
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        Console.Write($"{matrix[i , j], 6}");
+    }
+    Console.WriteLine("   |");
+    }
+}
+
