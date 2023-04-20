36af456912f4ad35fb23f1bf18324ed431f73c6a
PAUL AGUREEFF <p.agureev@mail.ru>
2023-04-20 11:19:05 +0300

    Задача 67. Работает. Рекурсия. Сумма цифр введенного числа

---
+++ TASK67/Program.cs
@@ -0,0 +1,25 @@
+﻿// int FactorialRec(int n)
+//  {
+//      if (n == 1) return 1;
+//      else return n * FactorialRec(n - 1);
+//  }
+
+//  Console.WriteLine(FactorialRec(10)); // 3628800
+// Задача 67: Напишите программу, которая будет принимать на вход число и
+// возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9
+// Задача 69: Напишите программу, которая на вход принимает два числа A и B, и
+// возводит число А в целую степень B с помощью рекурсии.
+// A = 3; B = 5 -> 243 (3⁵)
+// A = 2; B = 3 -> 8
+
+Console.WriteLine("Введите целое положительное число N");
+int number = Convert.ToInt32(Console.ReadLine());
+int totalResult = SummOfDigitsOfNumber(number);
+Console.WriteLine(totalResult);
+int SummOfDigitsOfNumber(int num)
+{
+    if (num == 0) return 0;
+    else return num%10+SummOfDigitsOfNumber(num/10);
+}
