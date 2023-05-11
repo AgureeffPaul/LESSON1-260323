68f02f33d1a3103c7644864f1f6426ff8ce63eec
PAUL AGUREEFF <p.agureev@mail.ru>
2023-05-11 09:56:51 +0300

    Домашка рекурсия. Задача 68

--- TASK68/Program.cs
+++ TASK68/Program.cs
@@ -2,28 +2,29 @@
 // m = 2, n = 3 -> A(m,n) = 9
 // m = 3, n = 2 -> A(m,n) = 29
 
-using System;
 
-class Program {
-    static void Main(string[] args) {
-        int m, n;
-        Console.Write("Введите значение m: ");
-        m = int.Parse(Console.ReadLine());
-        Console.Write("Введите значение n: ");
-        n = int.Parse(Console.ReadLine());
-        Console.WriteLine("Результат: " + AckermannFunction(m, n));
+Console.Write("Введите значение M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Результат: " + AckermannFunction(m, n));
+
+
+int AckermannFunction(int mM, int nN)
+{
+    if (mM == 0)
+    {
+        return nN + 1;
     }
-    
-    static int AckermannFunction(int m, int n) {
-        if (m == 0) {
-            return n + 1;
-        }
-        else if (n == 0) {
-            return AckermannFunction(m - 1, 1);
-        }
-        else {
-            return AckermannFunction(m - 1, AckermannFunction(m, n - 1));
-        }
+    else if (nN == 0)
+    {
+        return AckermannFunction(mM - 1, 1);
+    }
+    else
+    {
+        return AckermannFunction(mM - 1, AckermannFunction(mM, nN - 1));
     }
 }
+
 
