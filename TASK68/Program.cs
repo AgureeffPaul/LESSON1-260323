110de07aa8672e978bc28d62af08c15238fa25d6
PAUL AGUREEFF <p.agureev@mail.ru>
2023-05-11 11:57:22 +0300

    Задача 68 ДОМ. Функция Аккермана с помощью рекурсии. Добавил проверки. Работает

--- TASK68/Program.cs
+++ TASK68/Program.cs
@@ -2,14 +2,27 @@
 // m = 2, n = 3 -> A(m,n) = 9
 // m = 3, n = 2 -> A(m,n) = 29
 
+// A(0, 0) = 1
+// A(0, 1) = 2
+// A(0, 2) = 3
+// A(0, 3) = 4
+// A(0, 4) = 5
+// A(1, 0) = 2
+// A(2, 0) = 3
+// A(3, 0) = 5
+// A(4, 0) = 13
+// A(4, 1) = 65533
 
 Console.Write("Введите значение M: ");
 int m = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите значение N: ");
 int n = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("Результат: " + AckermannFunction(m, n));
-
+if (m >= 0 && n >= 0)
+{
+    Console.WriteLine("Результат: " + AckermannFunction(m, n));
+}
+else Console.WriteLine("Некорректный ввод");
 
 int AckermannFunction(int mM, int nN)
 {
