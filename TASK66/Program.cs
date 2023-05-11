067f416d8e729e08c91406c438be0ba8ee7c2d7a
PAUL AGUREEFF <p.agureev@mail.ru>
2023-05-11 09:44:28 +0300

    Домашка по рекурсии. Задача 64

---
+++ TASK66/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N. Выполнить с помощью рекурсии.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+using System;
+
+class Program {
+    static void Main(string[] args) {
+        Console.Write("Введите значение M: ");
+        int m = int.Parse(Console.ReadLine());
+        Console.Write("Введите значение N: ");
+        int n = int.Parse(Console.ReadLine());
+        int sum = SumRange(m, n);
+        Console.WriteLine("Сумма натуральных элементов: " + sum);
+    }
+    
+    static int SumRange(int start, int end) {
+        if (end == start) {
+            return end;
+        }
+        else {
+            return start + SumRange(start + 1, end);
+        }
+    }
+}
