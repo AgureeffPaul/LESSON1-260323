8dd1db6dea3749cadcc1dbb3e3a72dd44c0167ff
PAUL AGUREEFF <p.agureev@mail.ru>
2023-05-11 09:50:15 +0300

    Домашка рекурсия. Задача 66

--- TASK66/Program.cs
+++ TASK66/Program.cs
@@ -3,24 +3,23 @@
 // M = 1; N = 15 -> 120
 // M = 4; N = 8. -> 30
 
-using System;
+Console.Write("Введите значение M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int sum = SumRange(m, n);
 
-class Program {
-    static void Main(string[] args) {
-        Console.Write("Введите значение M: ");
-        int m = int.Parse(Console.ReadLine());
-        Console.Write("Введите значение N: ");
-        int n = int.Parse(Console.ReadLine());
-        int sum = SumRange(m, n);
-        Console.WriteLine("Сумма натуральных элементов: " + sum);
-    }
+Console.WriteLine("Сумма натуральных элементов: " + sum);
     
-    static int SumRange(int start, int end) {
-        if (end == start) {
-            return end;
-        }
-        else {
-            return start + SumRange(start + 1, end);
-        }
+int SumRange(int start, int end) 
+{
+    if (end == start) 
+    {
+    return end;
+    }
+    else 
+    {
+    return start + SumRange(start + 1, end);
     }
 }
+
